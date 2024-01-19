f8fc721b7a9e08aef24e5090a241400fcdfdf80d
Daniel Čech <daniel.cech@gmail.com>
2024-01-19 16:56:57 +0100

    feat: intensity & clean up

--- Arduino Project/OnAir/OnAir.ino
+++ Arduino Project/OnAir/OnAir.ino
@@ -6,11 +6,9 @@
 // How many NeoPixels are attached to the ESP8266?
 #define NUMPIXELS      24
 
+#define INTENSITY      0.6
+
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-
-int delayval = 500; // delay for half a second
-
-
 
 
 void setup() {
@@ -26,25 +24,11 @@ void loop() {
   double value = double(time) / 3000;
 
   for (int i = 0; i < NUMPIXELS; i++) {
-    uint8_t red = uint8_t(128 + 128 * sin(value + i * 0.02));
-    uint8_t green = uint8_t(128 + 128 * sin(value * i * 1.01 + i * 0.02));
-    uint8_t blue = uint8_t(128 + 128 * sin(value * 1.03 * i + i * 0.02));
+    uint8_t red = uint8_t(INTENSITY * (128 + 128 * sin(value + i * 0.02)));
+    uint8_t green = uint8_t(INTENSITY * (128 + 128 * sin(value * i * 1.01 + i * 0.02)));
+    uint8_t blue = uint8_t(INTENSITY * (128 + 128 * sin(value * 1.03 * i + i * 0.02)));
 
     pixels.setPixelColor(i, pixels.Color(red, green, blue)); // Moderately bright green color.
     pixels.show(); // This sends the updated pixel color to the hardware.
   }
-
-
-
-  // // put your main code here, to run repeatedly:
-  //  for(int i = 0; i < NUMPIXELS; i++){
-
-  //   // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-  //   pixels.setPixelColor(i, pixels.Color(0, 150, 0)); // Moderately bright green color.
-
-  //   pixels.show(); // This sends the updated pixel color to the hardware.
-
-  //   Serial.print("loop");
-  //   delay(delayval); // Delay for a period of time (in milliseconds).
-
 }
