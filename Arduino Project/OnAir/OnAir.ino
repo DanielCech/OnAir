82b4fc1abcbeb31bb02b1964da766221d5d4cfe2
Daniel Čech <daniel.cech@gmail.com>
2024-01-21 16:05:47 +0100

    feat: a small adjustments

--- Arduino Project/OnAir/OnAir.ino
+++ Arduino Project/OnAir/OnAir.ino
@@ -2,33 +2,40 @@
 
 // Which pin on the ESP8266 is connected to the NeoPixels?
 #define PIN            4
-
 // How many NeoPixels are attached to the ESP8266?
 #define NUMPIXELS      24
-
-#define INTENSITY      0.6
+#define INTENSITY      0.1
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-
+int offset;
+float intensity = 0.0;
 
 void setup() {
-  // put your setup code here, to run once:
   pixels.begin(); // This initializes the NeoPixel library.
   Serial.begin(115200);
+  offset = random(0, 9999);
 }
 
 void loop() {
-
   unsigned long time = millis();
+  double value = double(time + offset) / 3000;
 
-  double value = double(time) / 3000;
-
+  if (intensity < double(INTENSITY)) {
+    intensity = double(INTENSITY / 3000) * time;
+  }
+  
   for (int i = 0; i < NUMPIXELS; i++) {
-    uint8_t red = uint8_t(INTENSITY * (128 + 128 * sin(value + i * 0.02)));
-    uint8_t green = uint8_t(INTENSITY * (128 + 128 * sin(value * i * 1.01 + i * 0.02)));
-    uint8_t blue = uint8_t(INTENSITY * (128 + 128 * sin(value * 1.03 * i + i * 0.02)));
+    uint8_t red = uint8_t(intensity * (128 + 128 * sin(value + i * 0.02)));
+    uint8_t green = uint8_t(intensity * (128 + 128 * sin(value * i * 1.01 + i * 0.02)));
+    uint8_t blue = uint8_t(intensity * (128 + 128 * sin(value * 1.03 * i + i * 0.02)));
 
-    pixels.setPixelColor(i, pixels.Color(red, green, blue)); // Moderately bright green color.
+    pixels.setPixelColor(i, pixels.Color(red, green, blue));
     pixels.show(); // This sends the updated pixel color to the hardware.
   }
 }
+
+
+
+
+
+
